5683df185762a5393d786f24fb3d3a352483674b
Anastasia Boshniak <boshniak.a@gmail.com>
2020-01-31 18:32:39 +0200

    Submit finished

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 //Toggle menu button on click
-let menu_btn = document.getElementById('menu_btn');
-let menu_line = document.getElementById('menu_line');
+const menu_btn = document.getElementById('menu_btn');
+const menu_line = document.getElementById('menu_line');
+const submit_btn = document.getElementById("contact-form");
+const fieldName = document.getElementById("firstNameInput");
+const fieldEmail = document.getElementById("inputEmail");
+const fieldTel = document.getElementById("inputTel");
+
 
 menu_btn.addEventListener("click", toggleMenu);
 
@@ -9,7 +14,7 @@ function toggleMenu() {
 }
 
 //Validation Form
-let checkFunction = (input) => {
+const checkFunction = (input) => {
     const val = input.value;
 
     const errorText = document.getElementById("errorText");
@@ -22,9 +27,15 @@ let checkFunction = (input) => {
         if (nameRe.test(val)) {
             input.classList.add("invalid");
             errorText.innerHTML = "Please enter a valid name with alphabets only";
+            return false;
         } else if (val === '' || val == null) {
             input.classList.add("invalid");
+            console.log("err");
             errorText.innerHTML = "Required field";
+
+            return false;
+        } else {
+            return true;
         }
 //Validation Email Field
     } else if (input.type === "email") {
@@ -32,9 +43,13 @@ let checkFunction = (input) => {
         if (val === '' || val == null) {
             input.classList.add("invalid");
             errorEmail.innerHTML = "Required field";
+            return false;
         } else if ((re.test(val)) !== true) {
             input.classList.add("invalid");
             errorEmail.innerHTML = val + " is not valid. Please enter a valid e-mail address";
+            return false;
+        }   else {
+            return true;
         }
 //Validation Phone Field
     } else if (input.type === "tel") {
@@ -43,17 +58,21 @@ let checkFunction = (input) => {
             if (input.classList.contains('invalid')) {
                 input.classList.remove('invalid');
                 errorTel.innerHTML = "";
+                return false;
             }
         } else if (reg.test(val) !== true) {
             input.classList.add("invalid");
             errorTel.innerHTML = "Please enter a valid phone number xxx-xxx-xxxx";
+            return false;
+        }  else {
+            return true;
         }
     }
 
 
 };
 
-let correctFunction = (input) => {
+const correctFunction = (input) => {
     const errorText = document.getElementById("errorText");
     const errorEmail = document.getElementById("errorEmail");
     const errorTel = document.getElementById("errorTel");
@@ -78,3 +97,13 @@ let correctFunction = (input) => {
         }
     }
 };
+
+const submitFunction = (e) => {
+    e.preventDefault();
+    console.log("000");
+    if (!(!checkFunction(fieldName)&&!checkFunction(fieldEmail)&&!checkFunction(fieldTel))){
+      console.log("1");
+    }
+}
+
+submit_btn.addEventListener('submit', submitFunction) ;

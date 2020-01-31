806bfedd88e3478fd9e03ef1aec5be6434aedbb1
Anastasia Boshniak <boshniak.a@gmail.com>
2020-01-31 17:42:22 +0200

    Changed validation form and reset

--- js/main.js
+++ js/main.js
@@ -8,81 +8,73 @@ function toggleMenu() {
     menu_line.classList.toggle("menu_active");
 }
 
-//Validation form
-const name = document.getElementById("firstNameInput");
-const email = document.getElementById("inputEmail");
-const tel = document.getElementById("inputTel");
-const form = document.getElementById("contact-form");
+//Validation Form
+let checkFunction = (input) => {
+    const val = input.value;
 
+    const errorText = document.getElementById("errorText");
+    const errorEmail = document.getElementById("errorEmail");
+    const errorTel = document.getElementById("errorTel");
 
-//Creating error
-const node = document.createElement("span");
-
-// Validation field "Name"
-
-name.addEventListener('blur', () => {
-    let nameForm = document.getElementById("inputName");
-    let nameRe = /([^\sA-Za-z])+/g;
-    let value = name.value;
-    if (value === '' || value == null) {
-        node.innerHTML = "Required field";
-        nameForm.appendChild(node);
-    } else if (nameRe.test(value)) {
-        node.innerHTML = "Please enter a valid name with alphabets only";
-        nameForm.appendChild(node);
-    } else {
-        nameForm.removeChild(node);
-
+//Validation Name Field
+    if (input.type === "text") {
+        let nameRe = /([^\sA-Za-z])+/g;
+        if (nameRe.test(val)) {
+            input.classList.add("invalid");
+            errorText.innerHTML = "Please enter a valid name with alphabets only";
+        } else if (val === '' || val == null) {
+            input.classList.add("invalid");
+            errorText.innerHTML = "Required field";
+        }
+//Validation Email Field
+    } else if (input.type === "email") {
+        let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (val === '' || val == null) {
+            input.classList.add("invalid");
+            errorEmail.innerHTML = "Required field";
+        } else if ((re.test(val)) !== true) {
+            input.classList.add("invalid");
+            errorEmail.innerHTML = val + " is not valid. Please enter a valid e-mail address";
+        }
+//Validation Phone Field
+    } else if (input.type === "tel") {
+        let reg = /[0-9]{3}-[0-9]{3}-[0-9]{4}/;
+        if (val === '' || val == null) {
+            if (input.classList.contains('invalid')) {
+                input.classList.remove('invalid');
+                errorTel.innerHTML = "";
+            }
+        } else if (reg.test(val) !== true) {
+            input.classList.add("invalid");
+            errorTel.innerHTML = "Please enter a valid phone number xxx-xxx-xxxx";
+        }
     }
-});
 
 
-// Validation field "E-mail"
+};
 
-email.addEventListener('blur', () => {
-    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    let emailForm = document.getElementById("email");
-    let value = email.value;
-    if (value === '' || value == null) {
-        emailForm.appendChild(node);
-    } else if (re.test(value)) {
-        emailForm.removeChild(node);
-    } else {
-        node.innerHTML = value + " is not valid. Please enter a valid e-mail address";
-        emailForm.appendChild(node);
+let correctFunction = (input) => {
+    const errorText = document.getElementById("errorText");
+    const errorEmail = document.getElementById("errorEmail");
+    const errorTel = document.getElementById("errorTel");
+
+//Reset Name Field
+    if (input.type === "text") {
+        if (input.classList.contains('invalid')) {
+            input.classList.remove('invalid');
+            errorText.innerHTML = "";
+        }
+//Reset Email Field
+    } else if (input.type === "email") {
+        if (input.classList.contains('invalid')) {
+            input.classList.remove('invalid');
+            errorEmail.innerHTML = "";
+        }
+//Reset Phone Field
+    } else if (input.type === "tel") {
+        if (input.classList.contains('invalid')) {
+            input.classList.remove('invalid');
+            errorTel.innerHTML = "";
+        }
     }
-});
-
-// email.addEventListener('focus', ()=>{
-//     let nameForm = document.getElementById("email");
-//       if (email.nextElementSibling){
-//           node.innerHTML="";
-//           nameForm.removeChild(node);
-//       }
-// })  ;
-
-// Validation field "Phone"
-
-tel.addEventListener('blur', ()=>{
-    let value=tel.value;
-    let reg = /[0-9]{3}-[0-9]{3}-[0-9]{4}/;
-    let nameForm = document.getElementById('phone');
-    if (reg.test(value)!==true){
-        node.innerHTML = "Please enter a valid phone number xxx-xxx-xxxx";
-        nameForm.appendChild(node);
-    }  else{
-        nameForm.removeChild(node);
-    }
-})  ;
-
-// form.addEventListener('submit' , (e)=>{
-//     let messages = [];
-//     if (name.value == '' || name.value== null)      {
-//         messages.push('Name is required')
-//     }
-//
-//     if (messages.length>0){
-//         e.preventDefault();
-//         errorText.innerText = messages.join(', ');
-//     }
-// })  ;
+};
